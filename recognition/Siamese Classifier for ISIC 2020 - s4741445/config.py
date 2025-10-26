db3da9e2c176b707781fdb9c19bad2877d3daed0
Aryan-S-Gupta <aryansomesh.gupta@student.uq.edu.au>
2025-10-27 09:47:27 +1000

    add Drive root paths and dataset constants

---
+++ recognition/Siamese Classifier for ISIC 2020 - s4741445/config.py
@@ -0,0 +1,4 @@
+DRIVE_ROOT = "/content/drive/MyDrive/siamese_project/data"
+IMAGES_DIR = f"{DRIVE_ROOT}/train-image/image"
+META_CSV = f"{DRIVE_ROOT}/train-metadata.csv"
+ARTIFACTS = "/content/drive/MyDrive/siamese_project/outputs"
