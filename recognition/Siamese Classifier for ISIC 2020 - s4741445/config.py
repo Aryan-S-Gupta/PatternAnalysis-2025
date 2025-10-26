59306f30118bc0775499c895f68a4169ea0030c2
Aryan-S-Gupta <aryansomesh.gupta@student.uq.edu.au>
2025-10-27 09:49:29 +1000

    define base hyperparams and random seed

--- recognition/Siamese Classifier for ISIC 2020 - s4741445/config.py
+++ recognition/Siamese Classifier for ISIC 2020 - s4741445/config.py
@@ -2,3 +2,10 @@ DRIVE_ROOT = "/content/drive/MyDrive/siamese_project/data"
 IMAGES_DIR = f"{DRIVE_ROOT}/train-image/image"
 META_CSV = f"{DRIVE_ROOT}/train-metadata.csv"
 ARTIFACTS = "/content/drive/MyDrive/siamese_project/outputs"
+
+IMAGE_SIZE   = 224
+BATCH_SIZE   = 128
+NUM_WORKERS  = 0
+VAL_FRACTION = 0.15
+TEST_FRACTION= 0.15
+SEED         = 42
